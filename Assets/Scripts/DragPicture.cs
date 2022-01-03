52a753af7a68cb95ea2898596e04559d0debf056
Netherman2440 <zajacignacy@gmail.com>
2022-01-03 11:52:32 +0100

    Otwieranie i zamykanie tablicy

--- Assets/Scripts/DragPicture.cs
+++ Assets/Scripts/DragPicture.cs
@@ -13,7 +13,13 @@ public class DragPicture : MonoBehaviour, IDragHandler
     private RectTransform PictureToDrag;
    
     public void OnDrag(PointerEventData eventData)
-    {   //Pozycja zdjêcie zmienia siê o pozycjê myszki
+    {   /*
+         Pozycja zdjêcie zmienia siê o pozycjê myszki
+        Minimalne ró¿nice w przeci¹ganiu myszk¹, delikatnie myszka rozje¿d¿a siê ze zdjêciem
+         Na necie znalaz³em jakiœ poradnik z gotowym kodem, ale nie chce mi siê tego teraz sprawdzaæ wiêc sobie zostawie tu link jakby
+        nam jednak przeszkadza³o to przesuwanie
+        https://www.youtube.com/watch?v=Mb2oua3FjZg
+         */
         PictureToDrag.anchoredPosition += eventData.delta;
     }
 

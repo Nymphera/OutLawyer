8a551c1ca8e574c68ec82a1c97f11ac4c74db4b9
Netherman2440 <zajacignacy@gmail.com>
2022-01-05 00:30:12 +0100

    Zdjęcia wracają do ostatniej pozycji przed "Dragiem"

--- Assets/Scripts/DragPicture.cs
+++ Assets/Scripts/DragPicture.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 
-public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler,IBeginDragHandler,IEndDragHandler
+public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler,IBeginDragHandler
 
 
 //Zdjêcie z tym skryptem bêdzie reagowaæ na przesuniêcie myszy.
@@ -17,30 +17,21 @@ public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler,IBegi
     private Vector3 velocity = Vector3.zero;
     private Vector3 LastPosition;
     public void OnBeginDrag(PointerEventData eventData)
-    {
+    {   
+        if(IsInBorder(PinBoard,Evidence))
          LastPosition=Evidence.anchoredPosition;
-        print(LastPosition);
     }
-
     public void OnDrag(PointerEventData eventData)
     {
         if (Input.GetKey(KeyCode.Mouse0))
             if (IsInBorder(PinBoard, Evidence))
-            {
+            {//przyporz¹dkowuje zdjêciu pozycjê myszki jeœli ta jest na tablicy
                 Evidence.anchoredPosition += eventData.delta;
             }
+        //zdjêcie wraca do ostatniej pozycji z wartoœci¹ true
             else if (IsInBorder(PinBoard, Evidence) == false)
                 Evidence.anchoredPosition = LastPosition; 
 
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {// Jeœli zdjêcie wyjdzie poza granice to restartuje jego pozycjê
-        { }
-            
-        
-        
-      
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -49,14 +40,12 @@ public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler,IBegi
     }
     
     private bool IsInBorder( RectTransform PinBoard, RectTransform Evidence)
-    {/*
-      BUG: Na krawêdziach zdjêcia wariuj¹ i mo¿na zbugowaæ jak siê d³ugo trzyma przycisk, albo zmienia rozdizelczoœæ na 4k
-      */
+    {//sprawdza czy Zdjêcie le¿y na Pinboardzie
        Vector3 UpperLimit= ReturnCorners(PinBoard,2);
         Vector3 LowerLimit = ReturnCorners(PinBoard, 0);
         Vector3 EvidenceLowerLimit = ReturnCorners(Evidence, 0);
         Vector3 EvidenceUpperLimit = ReturnCorners(Evidence, 2);
-        Vector3 EvidencePosition = Evidence.position;
+       
         if (EvidenceLowerLimit.x >=LowerLimit.x && EvidenceLowerLimit.y >= LowerLimit.y && EvidenceUpperLimit.x <= UpperLimit.x && EvidenceUpperLimit.y <= UpperLimit.y)
         {
             return true;

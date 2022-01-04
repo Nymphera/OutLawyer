9e38a94a1f51302839fafa44a8166af2d16c7d42
Netherman2440 <zajacignacy@gmail.com>
2022-01-04 11:34:32 +0100

    Zdjęcia nie spierdalają

--- Assets/Scripts/DragPicture.cs
+++ Assets/Scripts/DragPicture.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler
 
 
@@ -11,21 +12,43 @@ public class DragPicture : MonoBehaviour, IDragHandler,IPointerDownHandler
 {
     [SerializeField]
     private RectTransform PictureToDrag;
-   
+    [SerializeField]
+    private RectTransform PinBoard;
+  
     public void OnDrag(PointerEventData eventData)
-    {   /*
-         Pozycja zdjêcie zmienia siê o pozycjê myszki
-        Minimalne ró¿nice w przeci¹ganiu myszk¹, delikatnie myszka rozje¿d¿a siê ze zdjêciem
-         Na necie znalaz³em jakiœ poradnik z gotowym kodem, ale nie chce mi siê tego teraz sprawdzaæ wiêc sobie zostawie tu link jakby
-        nam jednak przeszkadza³o to przesuwanie
-        https://www.youtube.com/watch?v=Mb2oua3FjZg
-         */
-        PictureToDrag.anchoredPosition += eventData.delta;
+    { if(Input.GetKey(KeyCode.Mouse0))
+        if (IsInBorder(Input.mousePosition, PinBoard))
+        {
+            PictureToDrag.anchoredPosition += eventData.delta;
+        }
     }
-
     public void OnPointerDown(PointerEventData eventData)
-    { //przerzuca zdjêciê na górê, ¿eby je by³o widaæ
+    { //przerzuca zdjêciê na górê stosu, ¿eby je by³o widaæ
         PictureToDrag.SetAsLastSibling();
+    }
+    
+    private bool IsInBorder(Vector3 MousePosition, RectTransform PinBoard)
+    {/*
+      BUG: Na krawêdziach zdjêcia wariuj¹ i mo¿na zbugowaæ jak siê d³ugo trzyma przycisk, albo zmienia rozdizelczoœæ na 4k
+      */
+       Vector3 UpperLimit= ReturnCorners(PinBoard,2);
+        Vector3 LowerLimit = ReturnCorners(PinBoard, 0);
+        if (MousePosition.x >=LowerLimit.x && MousePosition.y >= LowerLimit.y && MousePosition.x < UpperLimit.x && MousePosition.y < UpperLimit.y)
+        {
+            return true;
+        }   
+            else
+        {
+            return false;
+        }
+        // ReturnCorners() zwraca Rogi Tablicy Korkowej
+        Vector3 ReturnCorners(RectTransform PinBoard, int whichCorner)
+        {  
+            Vector3[] Corners = new Vector3[4];
+            PinBoard.GetWorldCorners(Corners);
+            return Corners[whichCorner];
+        }
+
     }
 }
 

0a120941d385f674c9ae75c758dfdd4ed47de337
Netherman2440 <zajacignacy@gmail.com>
2022-01-20 16:26:29 +0100

    Tablica wyciągana na wciśnięcie guzika

--- Assets/Scripts/CinemachineSwitcher.cs
+++ Assets/Scripts/CinemachineSwitcher.cs
@@ -7,21 +7,7 @@ public class CinemachineSwitcher : MonoBehaviour
 {   [SerializeField]
     private Animator Animator;
     private bool MainCameraState = true;
-    private Button PinboardButton;
-    void Start()
-    {
-        PinboardButton = PinboardButton.GetComponent<Button>();
-        PinboardButton = GameObject.Find("PinBoard Button").GetComponent<Button>();
-    }
-   
-    private void Update()
-    {
-        if (PinboardButton.IsActive())
-        {
-            SwitchState();
-        }
-    }
-   
+
     
     public void SwitchState()
     {

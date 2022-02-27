268483a1fcf9f6de02700287e5d6b7c5875cf2d2
Netherman2440 <zajacignacy@gmail.com>
2022-02-27 23:30:48 +0100

    Skrypt z ruszaniem kamerą po tablicy

--- Assets/Scripts/PinBoardCamera.cs
+++ Assets/Scripts/PinBoardCamera.cs
@@ -1,20 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
 
 public class PinBoardCamera : MonoBehaviour
-{[SerializeField]
-    private float CameraSpeed=20f;
-
+{
     
+    
+    [SerializeField]
+    private float CameraSpeed=20f;
+    [SerializeField]
+    private float FieldOfView = 40f;
+    [SerializeField]
+    private Vector3 FollowOffset;
+
+    private CinemachineInputProvider InputProvider;
+    private CinemachineVirtualCamera Camera;
+    private Transform cameraTransform; 
+  
+    
+    private void Awake()
+    {
+        InputProvider = GetComponent<CinemachineInputProvider>();
+        Camera = GetComponent<CinemachineVirtualCamera>();
+        cameraTransform = Camera.VirtualCameraGameObject.transform;
+
+        FieldOfView = Camera.m_Lens.FieldOfView;
+       
+    }
+
     void Update()
     {
-        Vector3 position = transform.position;
+        float x = InputProvider.GetAxisValue(0);
+        float y = InputProvider.GetAxisValue(1);
+        float z = InputProvider.GetAxisValue(2);
 
-        if (true)
+        if (x != 0 || y != 0)
         {
-            position.y += CameraSpeed * Time.deltaTime;
+            MoveCamera(x, y);
         }
-        transform.position = position;
+        
+       
+    }
+
+   
+    
+    
+    
+    public Vector2 MoveDirection(float x,float y)
+    {
+        Vector2 direction = Vector2.zero;
+        if (y>Screen.height*0.95f)
+        {
+            direction.y += 1;
+        }
+        if (y < Screen.height * 0.05f)
+        {
+            direction.y += -1;
+        }
+        if (x > Screen.width * 0.95f)
+        {
+            direction.x += 1;
+        }
+        if (x < Screen.width * 0.05f)
+        {
+            direction.x += -1;
+        }
+
+        return direction;
+    }
+    
+    
+    public void MoveCamera(float x, float y)
+    {
+        Vector2 Direction = MoveDirection(x, y);
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, (Vector3)Direction*CameraSpeed + cameraTransform.position,Time.deltaTime);
+        Debug.Log(Direction);
     }
 }

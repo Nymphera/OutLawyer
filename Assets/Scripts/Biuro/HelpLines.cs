997242bd1893ad481c7c65c78a25bb0b6392883c
Netherman2440 <zajacignacy@gmail.com>
2022-03-25 14:55:07 +0100

    nowa logika, jeszcze bez granic dla przycisków

--- Assets/Scripts/Biuro/HelpLines.cs
+++ Assets/Scripts/Biuro/HelpLines.cs
@@ -86,6 +86,25 @@ public class HelpLines : MonoBehaviour
                  }
              }
             LineCounter(line);
+        if (AreAllConectionsGood())
+        {
+            Debug.Log("You did gooood");
+        }
+    }
+
+    private bool AreAllConectionsGood()
+    { bool returnValue=true;
+        for(int i = 0; i < lines.Count; i++)
+        {
+            if (lines[i].isConectionGood != true)
+            {
+                returnValue= false;
+                Debug.Log("not yet");
+                break;
+            }
+            
+        }
+        return returnValue;
     }
 
     private void Start()
